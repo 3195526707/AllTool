af1593ae43416ded6b4a278d76af56a57e821f99
haizi <3195526707@qq.com>
2016-04-24 01:23:27 +0800

    My Commit Message

---
+++ app/src/main/java/com/haizi/alltool/base/BaseApplication.java
@@ -0,0 +1,14 @@
+package com.haizi.alltool.base;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2016/4/24.
+ */
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
