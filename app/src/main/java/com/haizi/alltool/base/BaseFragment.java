d76be4f0c68a3bb0bccb258093dce30fa5b8811e
haizi <3195526707@qq.com>
2016-04-24 20:57:24 +0800

    My Commit Message

--- app/src/main/java/com/haizi/alltool/base/BaseFragment.java
+++ app/src/main/java/com/haizi/alltool/base/BaseFragment.java
@@ -3,18 +3,39 @@ package com.haizi.alltool.base;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.haizi.alltool.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
  * Created by Administrator on 2016/4/24.
  */
-public class BaseFragment extends Fragment{
+public class BaseFragment extends Fragment {
+
+    private LayoutInflater inflater;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        this.inflater = inflater;
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    public View inflate(ViewGroup container) {
+        View view = inflater.inflate(R.layout.activity_main, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
     }
 }
