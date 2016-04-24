1a0b942ace176b85d7e6fbfc984acea8ebf9567b
haizi <3195526707@qq.com>
2016-04-24 19:57:07 +0800

    My Commit Message

--- app/src/main/java/com/haizi/alltool/MainActivity.java
+++ app/src/main/java/com/haizi/alltool/MainActivity.java
@@ -1,13 +1,24 @@
 package com.haizi.alltool;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
 import com.haizi.alltool.base.BaseAvtivity;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends BaseAvtivity {
+
+    @Bind(R.id.rv_function_list)
+    RecyclerView rvFunctionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
+
 }
--- app/src/main/java/com/haizi/alltool/base/BaseAvtivity.java
+++ app/src/main/java/com/haizi/alltool/base/BaseAvtivity.java
@@ -3,7 +3,11 @@ package com.haizi.alltool.base;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by Administrator on 2016/4/24.
@@ -14,5 +18,23 @@ public class BaseAvtivity extends FragmentActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public void setContentView(View view) {
+        super.setContentView(view);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public void setContentView(View view, ViewGroup.LayoutParams params) {
+        super.setContentView(view, params);
+        ButterKnife.bind(this);
     }
 }
